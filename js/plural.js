de7df735d4c3da9d3d95d502ae4a2eeaa8372dd4
asultan7890 <42124049+asultan7890@users.noreply.github.com>
2018-08-11 17:58:40 -0400

    add output for words ending in 'y'

--- js/plural.js
+++ js/plural.js
@@ -10,8 +10,30 @@ $(document).ready(function(){
           //change word to character array
           var characters = [...add_data];
 
+          //get string length
+          var len = add_data.length;
+
+          //initialize new word
+          var new_word = add_data
+
+          //if words ends in y
+          if (characters[len-1] == "y"){
+            if ($.inArray((characters[len-2]), ['a', 'e', 'i', 'o', 'u']) >= 0){
+              new_word = add_data + 's';
+            }
+            else {
+              new_word = add_data.substr(0, (len-1)) + "ies";
+            }
+          }
+
+          //
+          else if ((characters[len-1] == "f") || ((characters[len-1] == 'e') && (characters[len-2] == 'f'))){
+
+          }
+
+
           //display plural word
-          $('#list').prepend("<li>" +add_data+ " -> " +characters[0]+ "</li")
+          $('#list').prepend("<li>" +add_data+ " -> " +new_word+ "</li")
 
           //make text field blank
           $("#add_text").val("");
