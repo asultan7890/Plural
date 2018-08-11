6ed710fe9d1189a29bcecd56ab0ea9ef03e70a30
asultan7890 <42124049+asultan7890@users.noreply.github.com>
2018-08-11 17:25:39 -0400

    get user input

--- js/plural.js
+++ js/plural.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
 
-    
+  $("#add_text").keyup(function(evt){
+      //adds new item on "enter" press
+      if(evt.which === 13){
+
+          //get input word
+          var add_data = $("#add_text").val();
+
+          //change word to character array
+          var characters = [...add_data];
+
+          //display plural word
+          $('#list').prepend("<li>" +add_data+ " -> " +characters[0]+ "</li")
+
+          //make text field blank
+          $("#add_text").val("");
+        }
+      });
 });
