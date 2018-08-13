35c29d90acb51b6ed6e8c11ab44244e6767d1017
asultan7890 <42124049+asultan7890@users.noreply.github.com>
2018-08-13 10:53:16 -0400

    update definitions

--- js/plural.js
+++ js/plural.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 
           //get input word
           var add_data = $("#add_text").val();
+          add_data = add_data.toLowerCase();
 
           //change word to character array
           var characters = [...add_data];
@@ -16,7 +17,7 @@ $(document).ready(function(){
           //initialize new word
           var new_word = add_data
 
-          //if words ends in y
+          //if word ends in y
           if (characters[len-1] == "y"){
             if ($.inArray((characters[len-2]), ['a', 'e', 'i', 'o', 'u']) >= 0){
               new_word = add_data + 's';
@@ -26,9 +27,26 @@ $(document).ready(function(){
             }
           }
 
-          //
-          else if ((characters[len-1] == "f") || ((characters[len-1] == 'e') && (characters[len-2] == 'f'))){
+          //word ends in f
+          else if ((characters[len-1] == "f"){
+            if (characters[len-2] == 'l'){
+              if ($.inArray((characters[len-3]), ['a', 'e', 'o']) >= 0){
+                new_word = add_data.substr(0, (len-1)) + "ves";
+              }
+            }
+            else if ((characters[len-2] == 'a') && (characters[len-3] == 'e')){
+              new_word = add_data.substr(0, (len-1)) + "ves";
+            }
+          }
 
+          //word ends in fe
+          else if ((characters[len-1] == 'e') && (characters[len-2] == 'f'))){
+
+          }
+
+          //every other word
+          else{
+            new_word = add_data + 's';
           }
 
 
